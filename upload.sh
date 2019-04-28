aaaac32512c9417828a6cd90e5cce8cacfebb939
Yijun Yu <y.yu@open.ac.uk>
2019-04-29 00:00:41 +0100

    cleanup

--- upload.sh
+++ upload.sh
@@ -1,3 +1,4 @@
+rm -rf node_modules client/node_modules server/node_modules
 npm install path child_process tempfile tsc fs vscode vscode-languageserver vscode vscode-languageserver vscode vscode-languageserver vscode vscode-languageserver
 cd client && npm install child_process && npm run update-vscode && cd .. && npm run compile
 vsce package
